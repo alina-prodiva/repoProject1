d7bd1c3291b5d34f68b813a22f62a984c22c439f
Alina <alina@adzhieva.ru>
2025-01-23 08:26:50 +0300

    AddEditForm

--- main.py
+++ main.py
@@ -1,25 +1,103 @@
-from PyQt6.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableView, QWidget, QPushButton
+from PyQt6.QtSql import QSqlDatabase, QSqlTableModel, QSqlRecord, QSqlQuery
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableView, QWidget, QPushButton, QDialog, QMessageBox
+from PyQt6.QtCore import Qt
 from PyQt6 import uic
 import sys
 
 
+def neNone(n):
+    if len(n) == 0:
+        return ''
+    else:
+        return str(n)
+
+
+class AddEditWidget(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        self.stepen = None
+        self.name = None
+        self.price = None
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.con = QSqlDatabase.addDatabase('QSQLITE')
+        self.con.setDatabaseName('coffee.sqlite')
+        self.flag = ''
+        self.id.setEnabled(False)
+        self.zerno.addItems(['молотый', 'в зернах'])
+        self.save.clicked.connect(self.onSave)
+        self.cancel.clicked.connect(self.onCancel)
+
+    def onSave(self):
+        if self.flag == 'add':
+            self.con.open()
+            cur = QSqlQuery()
+            cur.exec(f"""insert into cofee (name, stepen, zerno, vkus, price, objem) 
+                        values ('{self.name.text()}', '{self.stepen.text()}', '{self.zerno.currentText()}', 
+                        '{self.vkus.text()}', '{float(self.price.text())}', '{self.objem.text()}')""")
+            self.con.close()
+            self.parent.show()
+            self.parent.onShowdata()
+            self.destroy()
+        else:
+
+            self.name.text()
+            self.con.open()
+            cur = QSqlQuery()
+            cur.exec(f"""update cofee set name = '{self.name.text()}', stepen = '{self.stepen.text()}', 
+                        zerno = '{self.zerno.currentText()}', vkus = '{self.vkus.text()}', price = '{float(self.price.text())}', 
+                        objem = '{self.objem.text()}'
+                        where id = '{int(self.id.text())}'""")
+            self.con.close()
+            self.parent.show()
+            self.parent.onShowdata()
+            self.destroy()
+
+    def onCancel(self):
+        self.parent.show()
+        self.destroy()
+
+
 class MyWidget(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super(MyWidget, self).__init__()
         uic.loadUi('main.ui', self)
-        self.pushButton.clicked.connect(self.run)
+        self.con = QSqlDatabase.addDatabase('QSQLITE')
+        self.con.setDatabaseName('coffee.sqlite')
+        self.showdata.clicked.connect(self.onShowdata)
+        self.adddata.clicked.connect(self.onAdddata)
+        self.editdata.clicked.connect(self.onEditdata)
+        self.exit.clicked.connect(self.onExit)
 
-    def run(self):
-        db = QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName('coffee.sqlite')
-        db.open()
-        model = QSqlTableModel(self, db)
-        model.setTable('cofee')
-        model.select()
-        self.tableView.setModel(model)
+    def onShowdata(self):
+        self.con.open()
+        self.model = QSqlTableModel(self, self.con)
+        self.model.setTable('cofee')
+        self.model.select()
+        self.con.close()
+        self.tableView.setModel(self.model)
         ex.update()
 
+    def onAdddata(self):
+        self.frame = AddEditWidget(self)
+        self.frame.flag = 'add'
+        self.frame.show()
+
+    def onEditdata(self):
+        self.frame = AddEditWidget(self)
+        self.frame.flag = 'edit'
+        index = (self.tableView.selectionModel().currentIndex())
+        self.frame.id.setText(str(index.sibling(index.row(), 0).data()))
+        self.frame.name.setText(index.sibling(index.row(), 1).data())
+        self.frame.stepen.setText(index.sibling(index.row(), 2).data())
+        self.frame.zerno.setCurrentText(index.sibling(index.row(), 3).data())
+        self.frame.vkus.setText(index.sibling(index.row(), 4).data())
+        self.frame.price.setText(str(index.sibling(index.row(), 5).data()))
+        self.frame.objem.setText(str(index.sibling(index.row(), 6).data()))
+        self.frame.show()
+
+    def onExit(self):
+        sys.exit(app.exec())
 
 
 if __name__ == '__main__':
